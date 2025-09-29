fcf7a8cc1a2a86a8a029eaf9ab1cd8ce77179097
SHEMNDARONGUGI <ndaroshem@gmal.com>
2025-09-30 01:03:30 +0300

    Initial commit

--- basics/app.js
+++ basics/app.js
@@ -8,10 +8,51 @@
 //Import functionality
 const PORT = 3000;
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
+    //parsing the urls
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    //to quit execution
+    return res.end();
+    }
     // process.exit();
+
+    
+    if(url === '/message' && method==='POST'){
+        const body = [];
+        //event listener on data event
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(parsedBody);
+            fs.writeFileSync('message.txt', message);
+        });
+        
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
+    //setting our own header
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first</title></head>');
+    res.write('<body><h3>Its interesting to learn node.js</h3></body>');
+    res.write('</html>');
+    res.end();
 });
 
 // will keep on listening for request
@@ -30,3 +71,5 @@ server.listen(PORT);
 //Node.js program Lifesycle
 
 // node app.js => start script => Parse Code, Register Variables and Functions => The Node Application [Event Loop] > Keeps on running as long as there are event listeners registered
+
+//Streams and Buffers
